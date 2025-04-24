f3442b0bd6f6d4ca1178c3b7d7006b118751b3dc
Jakub Siedlecki <jakub.siedlecki@infotech.edu.pl>
2025-04-24 12:17:29 +0200

    redmi

--- main.sh
+++ main.sh
@@ -7,3 +7,6 @@ echo "W królestwie Eldoria od setek lat panował pokój. Prastary Las Ciernisty
 Jednak od kilku tygodni wioski na obrzeżach lasu są atakowane przez dziwne bestie. Znikają ludzie. Cienie przemieszczają się za dnia, a księżyc przybiera krwistą barwę. Król Eldarion, zaniepokojony wydarzeniami, wysyła zwiadowców — żaden nie wraca.
 
 W tej sytuacji pojawiasz się Ty — młody wojownik, mag, złodziej (lub kimkolwiek gracz zdecyduje się być), który otrzymuje tajemnicze wezwanie od dawno zaginionego mentora. Zostajeś wciągnięty w wir wydarzeń, które mogą zadecydować o losie całego królestwa. "
+
+echo "Wybiesz 1 aby zacząć"
+echo ""
