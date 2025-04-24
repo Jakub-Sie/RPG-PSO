5e524644d365f3be9dc51354505d16542e25ec2d
Jakub Siedlecki <jakub.siedlecki@infotech.edu.pl>
2025-04-24 12:38:50 +0200

    menu

--- main.sh
+++ main.sh
@@ -9,4 +9,33 @@ Jednak od kilku tygodni wioski na obrzeżach lasu są atakowane przez dziwne bes
 W tej sytuacji pojawiasz się Ty — młody wojownik, mag, złodziej (lub kimkolwiek gracz zdecyduje się być), który otrzymuje tajemnicze wezwanie od dawno zaginionego mentora. Zostajeś wciągnięty w wir wydarzeń, które mogą zadecydować o losie całego królestwa. "
 
 echo "Wybiesz 1 aby zacząć"
-echo ""
+echo "Wybierz 2 aby wyjść "
+
+read znak
+
+case $znak in
+    1)
+    echo "Jaką klasą postaci chcesz grać"
+    echo "1] Żołniesz"
+    echo "2] Mag"
+    echo "3] Złodzej"
+
+    read znak
+
+    case $znak in 
+        1) posta=1
+        echo " Dziękuje za wybur zaczynamy grać"
+        clear ;;
+
+        2) posta=2
+        echo " Dziękuje za wybur zaczynamy grać"
+        clear ;;
+
+        3) posta=3
+        echo " Dziękuje za wybur zaczynamy grać"
+        clear ;;
+
+        
+    esac
+    echo "$posta"
+esac
