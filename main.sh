88135a7a5b22b8e8c5a99856fc49bb317ce7f72b
Jakub Siedlecki <jakub.siedlecki@infotech.edu.pl>
2025-04-24 12:15:27 +0200

    tytuł

--- main.sh
+++ main.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+echo "Hej tu jestes wspaniała gra w bash"
+echo "Tytuł : Cień Królewskiego Lasu" 
+echo "W królestwie Eldoria od setek lat panował pokój. Prastary Las Ciernistych Korzeni oddzielał granice królestwa od dzikich ziem, a legendy głosiły, że w jego sercu spoczywa starożytne zło – dawno uśpione przez czarodziejów Zakonu Białej Róży.
+
+Jednak od kilku tygodni wioski na obrzeżach lasu są atakowane przez dziwne bestie. Znikają ludzie. Cienie przemieszczają się za dnia, a księżyc przybiera krwistą barwę. Król Eldarion, zaniepokojony wydarzeniami, wysyła zwiadowców — żaden nie wraca.
+
+W tej sytuacji pojawiasz się Ty — młody wojownik, mag, złodziej (lub kimkolwiek gracz zdecyduje się być), który otrzymuje tajemnicze wezwanie od dawno zaginionego mentora. Zostajeś wciągnięty w wir wydarzeń, które mogą zadecydować o losie całego królestwa. "
