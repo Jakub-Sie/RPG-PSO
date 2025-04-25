5e0d0ccdb5cb45ba470c523f75e25b7dcfee05f7
battery-cell <leon.dembowski@infotech.edu.pl>
2025-04-25 09:59:02 +0200

    dodalem tyle ile moglem

--- main.sh
+++ main.sh
@@ -2,12 +2,13 @@
 
 echo "Hej tu jestes wspaniała gra w bash"
 echo "Tytuł : Cień Królewskiego Lasu" 
-echo "W królestwie Eldoria od setek lat panował pokój. Prastary Las Ciernistych Korzeni oddzielał granice królestwa od dzikich ziem, a legendy głosiły, że w jego sercu spoczywa starożytne zło – dawno uśpione przez czarodziejów Zakonu Białej Róży.
+echo "W królestwie Eldoria od setek lat panował pokój. \n Prastary Las Ciernistych Korzeni oddzielał granice królestwa od dzikich ziem, a legendy głosiły, że w jego sercu spoczywa starożytne zło – dawno uśpione przez czarodziejów Zakonu Białej Róży.
 
 Jednak od kilku tygodni wioski na obrzeżach lasu są atakowane przez dziwne bestie. Znikają ludzie. Cienie przemieszczają się za dnia, a księżyc przybiera krwistą barwę. Król Eldarion, zaniepokojony wydarzeniami, wysyła zwiadowców — żaden nie wraca.
 
 W tej sytuacji pojawiasz się Ty — młody wojownik, mag, złodziej (lub kimkolwiek gracz zdecyduje się być), który otrzymuje tajemnicze wezwanie od dawno zaginionego mentora. Zostajeś wciągnięty w wir wydarzeń, które mogą zadecydować o losie całego królestwa. "
-
+echo
+echo
 echo "Budzisz sie i wstajesz z luzka zakladajac kroksy i zakladajac spodnie razem z koszulka decydujesz sie na sniadanie w pol zaspanych myslach"
 sleep 20
 echo
@@ -26,22 +27,23 @@ if [ "$input" = "1" ]; then
 elif [ "$input" = "2" ]; then
     echo "nawet dobre!"
 elif [ "$input" = "3" ]; then
-    echo "rozpiera cie energia!"	
-else
-    echo "o co ci chodzi czlowieku dalaem ci 3 opcje a ty nawet tego nie umiesz zrobic ty szympansie zasrany"
-    
-if [ "$input" = "3" ]; then
-	echo "dobra co dalej"
+	echo "rozpiera cie energia!"
+    	echo "dobra co dalej"
 	echo
 	echo "1. zapale drugiego czemu nie"
 	echo
 	echo "2. dobra odpuszcze se"
 	read input2
-	if [ "$input" = "1" ]; then
-		echo "umarles na raka!"
+	if [ "$input2" = "1" ]; then
+		figlet umarles na raka!
 		fi
-	elif [ "$input" = "2" ]; then
-	echo "debug"
+	elif [ "$input2" = "2" ]; then
+		echo "debug"	
+else
+    echo "o co ci chodzi czlowieku dalaem ci 3 opcje a ty nawet tego nie umiesz zrobic ty szympansie zasrany"
+    
+
+
 
 
 
